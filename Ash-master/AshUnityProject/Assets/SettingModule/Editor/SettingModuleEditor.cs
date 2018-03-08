4f532eea18b36699ee33aab097e83ce371052bc8
huangjk <88386428@qq.com>
2018-03-08 09:20:00 +0800

    Revert "111"
    This reverts commit 1a8778b6820952c1e2ebfea788b17d71331c4fed.

---
+++ Ash-master/AshUnityProject/Assets/SettingModule/Editor/SettingModuleEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DotLiquid;
+using TableML.Compiler;
+using UnityEditor;
+using UnityEngine;
+
+namespace AppSettings
+{
+    /// <summary>
+    /// For SettingModule
+    /// </summary>
+    [InitializeOnLoad]
+    public class SettingModuleEditor
+    {
+        [MenuItem("KEngine/Settings/Force Compile Settings + Code")]
+        public static void CompileSettings()
+        {
+            DoCompileSettings(true);
+        }
+
+        [MenuItem("KEngine/Settings/Quick Compile Settings")]
+        public static void QuickCompileSettings()
+        {
+            DoCompileSettings(false);
+        }
+
+
+        public static void DoCompileSettings(bool force = true)
+        {
+            var bc = new BatchCompiler();
+
+            var sourcePath = "Product/SettingSource";
+            var compilePath = "Product/Setting";
+            var SettingCodePath = "Assets/AppSettings.cs";
+            var template = DefaultTemplate.GenCodeTemplate; // 
+            var SettingExtension = ".bytes";
+            var settingCodeIgnorePattern = "(I18N/.*)|(StringsTable.*)";
+            //var force = true;
+            var results = bc.CompileTableMLAll(sourcePath, compilePath, SettingCodePath, template, "AppSettings", SettingExtension, settingCodeIgnorePattern, force);
+
+            //            CompileTabConfigs(sourcePath, compilePath, SettingCodePath, SettingExtension, force);
+            var sb = new StringBuilder();
+            foreach (var r in results)
+            {
+                sb.AppendLine(string.Format("Excel {0} -> {1}", r.ExcelFile, r.TabFileRelativePath));
+            }
+            UnityEngine.Debug.Log("TableML all Compile ok!\n" + sb.ToString());
+            // make unity compile
+            AssetDatabase.Refresh();
+        }
+    }
+}
